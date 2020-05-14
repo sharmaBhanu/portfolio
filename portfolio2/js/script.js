bbaa0b5c4b33c2146eabd1353507a835e3b2dd13
sharmaBhanu <63666825+sharmaBhanu@users.noreply.github.com>
2020-05-15 01:20:31 +0530

    Add files via upload

---
+++ portfolio2/js/script.js
@@ -0,0 +1,14 @@
+(function ($) {
+    var typed = new Typed('span.txt-rotate',{
+        strings: ["I'm a web developer", "I'm a coder","I'm an Android developer", "I'm a software engineer"],
+        typespeed= 100,
+        backspeed= 100,
+        fadeout= false,
+        smartbackspace= true,
+        loop= true,
+
+
+    });
+    
+    (jquery)
+})
