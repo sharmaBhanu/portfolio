a308740281074df1cef4987b8f6fd62df2c45c69
hardikhere <web.hardikhere@gmail.com>
2020-05-15 08:02:27 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".text-rotate").typed({
+        strings: ["I'm a Web designer", "I'm a coder","I'm an Android developer", "I'm a software engineer","Thanks for visit"],
+        typeSpeed: 100,
+        backspeed:100,
+        fadeout: false,
+        smartbackspace: true,
+        loop:true,
+        cursorChar: "|"
+
+    });
+});
