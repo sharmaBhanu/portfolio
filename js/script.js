572dee219cf173fde3bb5f29a33adc0776757514
sharmaBhanu <63666825+sharmaBhanu@users.noreply.github.com>
2021-09-30 12:58:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".text-rotate").typed({
-        strings: ["I'm a Web designer", "I'm a coder","I'm an Android developer", "I'm a software engineer","Thanks for visit"],
+        strings: ["I'm a Web designer", "I'm a coder","I'm a Web developer", "I'm a software engineer","Microsoft Certified Azure Cloud Fundamentals","Thanks for visit"],
         typeSpeed: 100,
         backspeed:100,
         fadeout: false,
